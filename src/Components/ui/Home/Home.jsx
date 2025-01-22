26288fa59e7255f78f8ceb3268158908cd7ccd50
Muneera <muneerakottarath99@gmail.com>
2025-01-22 13:52:44 +0530

    second commit

--- src/Components/ui/Home/Home.jsx
+++ src/Components/ui/Home/Home.jsx
@@ -3,7 +3,7 @@ import Footer from '../Footer/Footer'
 import { Link } from 'react-router-dom'
 function Home() {
   return (
-    <div className='min-h-screen min-w-full homebg'>
+   <>
       <div className=' flex sm:flex-col-reverse lg:flex-row w-5/6 m-auto py-[3rem] min-h-screen gap-[36px]'>
         <div className='basis-1/2  flex flex-col  justify-center gap-[44px] sm:py-4 md:p-8  xl:p-14'>
              <div className='flex flex-col gap-[8px] '>
@@ -21,7 +21,7 @@ function Home() {
       
     </div>
 
-<Footer/></div>
+<Footer/></>
   )
 }
 
